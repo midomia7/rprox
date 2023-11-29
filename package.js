ac1029f5bcfc368e31f2b7c57086225cf391932c
midomia7 <54557447+midomia7@users.noreply.github.com>
2023-11-29 01:54:01 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "vercel-proxy",
+  "version": "1.0.0",
+  "description": "Proxy service deployed on Vercel",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "request-promise-native": "^1.0.9"
+  },
+  "engines": {
+    "node": "14.x"
+  },
+  "license": "ISC"
+}
